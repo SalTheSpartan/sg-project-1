620ad637322fbb4e98cad0ab078ae996ea773141
Salman Khan <skhan@spartaglobal.co>
2017-03-04 22:08:12 +0000

    added letter buttons

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+  't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var phrases = ['Liverpool', 'Barcelona' , 'AC Milan'];
 $(document).ready(function(){
-  $('p').click(function(){
-    $(this).hide();
-  });
+
+
 });
+
+
+
+$(document).ready(function(){
+  for (var i = 0; i < alphabet.length; i++) {
+    var $letterButton = $('<button></button>');
+    $letterButton.append(alphabet[i]);
+    $('.btn-group').append($letterButton);
+  }
+});
+
 
 
 
