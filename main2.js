ca51168927bb4d7b94b903d7d3d9beb2c245d906
Salman Khan <skhan@spartaglobal.co>
2017-03-07 13:52:40 +0000

    created category generator

--- main2.js
+++ main2.js
@@ -3,12 +3,37 @@ $(document).ready(function(){
     'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-  var phrases = ['LIVERPOOL', 'BARCELONA' , 'INTER MILAN'];
+  var categories = ['FOOTBALL TEAMS', 'MOVIES', 'PEOPLE'];
+  var footballPhrases = ['LIVERPOOL', 'BARCELONA' , 'INTER MILAN'];
+  var moviePhrases = ['LOGAN', 'SEABISCUIT', 'THE PURSUIT OF HAPPYNESS'];
+  var peoplePhrases = ['JAMES COLAIRO', 'SALMAN KHAN', 'OSVALDO CALIERI'];
+  var phrases = [];
   var blankField = [];
 
+  function categoryGenerator(){
+    var category = categories[Math.floor(Math.random() * categories.length)];
+    $('#category').append(category);
+    switch(category) {
+      case 'FOOTBALL TEAMS':
+        phrases = footballPhrases;
+        console.log(category);
+        break;
+      case 'MOVIES':
+        phrases = moviePhrases;
+        console.log(category);
+        break;
+      case 'PEOPLE':
+        phrases = peoplePhrases;
+        console.log(category);
+
+    }
+
+  }
+
+  categoryGenerator();
   function phraseGenerator(){
     var $phraseContent = $('<li></li>');
-    var number = 0 + Math.floor(Math.random() * 3);
+    var number = 0 + Math.floor(Math.random() * phrases.length);
     $phraseContent.append(phrases[number]);
     var phraseArray = phrases[number].split('');
     console.log(phraseArray);
@@ -18,7 +43,7 @@ $(document).ready(function(){
       $('.phrase').html('<span>' + blankField + '</span>');
 
     }
-    $('.phrase').append($phraseContent);
+    // $('.phrase').append($phraseContent);
     return phraseArray;
   }
 
@@ -27,7 +52,7 @@ $(document).ready(function(){
 
   function buttonGenerator(){
     for (var i = 0; i < alphabet.length; i++) {
-      var letterButton = $('<button></button>');
+      var letterButton = $('<button class="letterButton"></button>');
       letterButton.append(alphabet[i]);
       letterButton.prop('value', alphabet[i]);
       $('.btn-group').append(letterButton);
@@ -36,32 +61,36 @@ $(document).ready(function(){
 
   }
 
-
   buttonGenerator();
 
-  function remainigGuesses(){
-    // $('.usedLetters').append($(this).val());
+  function guessCounter(remainingGuesses){
+
+    var guessWrapper = $('<h3>'+ remainingGuesses + '<h3>');
+    $('.guessCounter').html(guessWrapper);
+
   }
 
   function guessPhrase(phraseArray, numberOfGuesses){
 
-    $('button').click(function() {
+    $('.letterButton').click(function() {
       $(this).attr('disabled', true);
       $(this).css('border', '1px solid red');
       numberOfGuesses -= 1;
+      guessCounter(numberOfGuesses);
       console.log(numberOfGuesses);
-      if (numberOfGuesses > 0){
-      for (var i = 0; i < phraseArray.length; i++){
-        if ($(this).val() === phraseArray[i]){
-          console.log(phraseArray[i]);
-          blankField[i] = phraseArray[i];
-          $('.phrase').html('<span>' + blankField + '</span>');
+      if (numberOfGuesses >= 0){
+        for (var i = 0; i < phraseArray.length; i++){
+          if ($(this).val() === phraseArray[i]){
+            console.log(phraseArray[i]);
+            blankField[i] = phraseArray[i];
+            $('.phrase').html('<span>' + blankField + '</span>');
 
-        }
-      } // end of for loop
-    }else{
-      console.log('game over');
-    }
+          }
+        } // end of for loop
+      }else{
+        console.log('game over');
+        guessCounter('You Lose, Loser!');
+      }
     });
 
   }
