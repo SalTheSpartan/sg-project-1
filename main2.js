df1a5eefa5bf8869ae18c738b7ce182e83811f4a
Salman Khan <skhan@spartaglobal.co>
2017-03-09 15:55:02 +0000

    added instructions link

--- main2.js
+++ main2.js
@@ -156,3 +156,8 @@ function isPhraseSolved(phraseArray, blankField){
     return true;
   }
 }
+
+function myFunction() {
+  var popup = document.getElementById('myPopup');
+  popup.classList.toggle('show');
+}
